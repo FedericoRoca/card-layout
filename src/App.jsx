1bc1abe7b9c01764bbfafee429012fecc29e26d3
FedericoRoca <roca.fedee@gmail.com>
2022-03-13 17:39:01 -0300

    card component added

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+import CardLayout from "./components/CardLayout";
+function App() {
+  return (
+    <div className="App text-center">
+      <CardLayout />
+    </div>
+  );
+}
+
+export default App;
